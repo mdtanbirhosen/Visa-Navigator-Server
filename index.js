849c6b16f7d34eacd9290b23b3c512f7028f0ba5
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2024-12-06 18:14:46 +0600

    applied visas delete  and all visa's delete operation done .

--- index.js
+++ index.js
@@ -25,47 +25,47 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-        
+
         //data base collections
         const database = client.db('VisasDB');
-        const allVisaCollection = database.collection('allVisas') 
-        const appliedVisaCollection = database.collection('appliedVisas') 
+        const allVisaCollection = database.collection('allVisas')
+        const appliedVisaCollection = database.collection('appliedVisas')
 
         // CRUD operations
-        app.get('/visas', async(req, res) => {
+        app.get('/visas', async (req, res) => {
             const cursor = allVisaCollection.find();
             const result = await cursor.toArray();
             res.send(result)
         })
 
         // get single visa for details
-        app.get('/visa/:id', async(req,res)=>{
+        app.get('/visa/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const result = await allVisaCollection.findOne(query)
             res.send(result)
         })
         // my added visas get operation
-        app.get('/visas/:email', async(req, res)=>{
+        app.get('/visas/:email', async (req, res) => {
             const email = req.params.email;
-            const query = {addByEmail: email};
-            const cursor =await allVisaCollection.find(query);
+            const query = { addByEmail: email };
+            const cursor = allVisaCollection.find(query);
             const result = await cursor.toArray()
             res.send(result)
         })
 
         // appliedVisas get operation
-        app.get('/appliedVisas/:email', async(req, res)=>{
+        app.get('/appliedVisas/:email', async (req, res) => {
             const email = req.params.email;
-            const query = {email: email};
-            const cursor =await appliedVisaCollection.find(query);
+            const query = { email: email };
+            const cursor = appliedVisaCollection.find(query);
             const result = await cursor.toArray()
             res.send(result)
         })
 
 
 
-        app.post('/visas', async(req, res)=>{
+        app.post('/visas', async (req, res) => {
             const newVisa = req.body;
             console.log("adding new visa", newVisa);
             const result = await allVisaCollection.insertOne(newVisa);
@@ -73,13 +73,51 @@ async function run() {
         })
 
         // appliedVisas post operation
-        app.post('/appliedVisas/:email', async(req, res)=>{
+        app.post('/appliedVisas', async (req, res) => {
             const newAppliedVisa = req.body;
             console.log("user applied for visa: ", newAppliedVisa);
             const result = await appliedVisaCollection.insertOne(newAppliedVisa);
             res.send(result)
         })
-
+        
+        
+        // update a visa using patch
+        app.patch('/visa', async (req, res) => {
+            const id = req.body._id;
+            const filter = { _id: new ObjectId(id) };
+            const updatedVisa = {
+                $set: {
+                    countryName: req.body?.countryName,
+                    countryImage: req.body?.countryImage,
+                    visaType: req.body?.visaType,
+                    processingTime: req.body?.processingTime,
+                    requiredDocuments: req.body?.requiredDocuments,
+                    description: req.body?.description,
+                    ageRestriction: req.body?.ageRestriction,
+                    fee: req.body?.fee,
+                    validity: req.body?.validity,
+                    applicationMethod: req.body?.applicationMethod,
+                }
+            }
+            const result = await allVisaCollection.updateOne(filter, updatedVisa);
+            res.send(result)
+        })
+        
+        // delete a visa from my added visas
+        app.delete('/visa/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await allVisaCollection.deleteOne(query);
+            res.send(result)
+        })
+        // appliedVisas post operation
+        app.delete('/appliedVisas/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await appliedVisaCollection.deleteOne(query);
+            res.send(result)
+        })
+        
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
         // console.log("Pinged your deployment. You successfully connected to MongoDB!");
